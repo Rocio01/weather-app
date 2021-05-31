358746fc874ee53d2076143d7d84b51b2f5560de
exit <zrmartinezg@gmail.com>
2021-05-30 22:01:33 -0500

    Refactor class Weather to add unit value

--- src/weather.js
+++ src/weather.js
@@ -1,15 +1,15 @@
 class Weather {
-  constructor(city) {
+  constructor(city, unit= "metric") {
     this.apiKey = "ca60cb74799b94c82b7e12865a1489c2";
     this.city = city;
-    this.unit = "C";
+    this.unit = unit;
   }
 
   async WeatherInformation() {
     const response =  await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=${this.unit}&APPID=${this.apiKey}`,
     { mode: 'cors' });
     const information = await response.json();
-    return information;
+    return information.weather[0].main;
   }
 
 }

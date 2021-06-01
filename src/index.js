d68ebc659424a5c9ba4e50e58f44ad59316a0b17
exit <zrmartinezg@gmail.com>
2021-06-01 15:07:01 -0500

    Fix stylelint, eslint errors

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
-import Weather from "./weather";
-import Display from "./ui";
+import Weather from './weather';
+import Display from './ui';
 
-
-const weather = new Weather("Bogota", "metric");
+const weather = new Weather('Bogota', 'metric');
 const ui = new Display();
 
-const buttonCheck = document.getElementById("city-form");
+const buttonCheck = document.getElementById('city-form');
 
 const weatherInformation = () => {
   weather.weatherInformation()
-  .then(results => {
-    ui.displayInfo(results);
-  })
-  .catch((e) => alert("city not found"));
+    .then((results) => {
+      ui.displayInfo(results);
+    })
+    .catch(alert('city not found'));
+};
 
-}
-
-buttonCheck.addEventListener("submit", (e) => {
+buttonCheck.addEventListener('submit', (e) => {
   e.preventDefault();
-  const city = document.getElementById("city").value;
-  const unit = document.getElementById("unit").value;
+  const city = document.getElementById('city').value;
+  const unit = document.getElementById('unit').value;
   weather.changeValues(city, unit);
   weatherInformation();
 });
--- src/ui.js
+++ src/ui.js
@@ -1,35 +1,32 @@
 class Display {
   constructor() {
-    this.city = document.getElementById("city-name");
-    this.weatherMain = document.getElementById("weather-main");
-    this.weatherDescription = document.getElementById("weather-description");
-    this.icon = document.getElementById("weather-icon");
-    this.temp = document.getElementById("weather-temp");
-    this.feelsLike = document.getElementById("weather-feelsLike");
-    this.humidity = document.getElementById("weather-humidity");
-    this.tempMax = document.getElementById("weather-temp_max");
-    this.tempMin = document.getElementById("weather-temp_min");
+    this.city = document.getElementById('city-name');
+    this.weatherMain = document.getElementById('weather-main');
+    this.weatherDescription = document.getElementById('weather-description');
+    this.icon = document.getElementById('weather-icon');
+    this.temp = document.getElementById('weather-temp');
+    this.feelsLike = document.getElementById('weather-feelsLike');
+    this.humidity = document.getElementById('weather-humidity');
+    this.tempMax = document.getElementById('weather-temp_max');
+    this.tempMin = document.getElementById('weather-temp_min');
   }
- 
 
-
-  displayInfo(weather){
+  displayInfo(weather) {
     const info = weather.weather[0];
     const fetchIcon = info.icon;
-    
-    const iconUrl = "http://openweathermap.org/img/wn/" + fetchIcon + ".png";
-    
-    this.city.innerHTML = `${weather.name} , ${weather.sys.country}.`; 
+
+    const iconUrl = `http://openweathermap.org/img/wn/${fetchIcon}.png`;
+
+    this.city.innerHTML = `${weather.name} , ${weather.sys.country}.`;
     this.weatherMain.innerHTML = info.main;
-    this.weatherDescription.innerHTML = `Description: ${info.description}.` ;
-    this.icon.setAttribute("src", iconUrl);
-    this.temp.innerHTML =  `Temperature: ${weather.main.temp}`;
+    this.weatherDescription.innerHTML = `Description: ${info.description}.`;
+    this.icon.setAttribute('src', iconUrl);
+    this.temp.innerHTML = `Temperature: ${weather.main.temp}`;
     this.feelsLike.innerHTML = `Feels like: ${weather.main.feels_like}`;
     this.humidity.innerHTML = `Humidity: ${weather.main.humidity}%`;
     this.tempMax.innerHTML = `Max temp at moment: ${weather.main.temp_max}`;
     this.tempMin.innerHTML = `Min temp at moment: ${weather.main.temp_min}`;
   }
-
 }
 
-export {Display as default}
+export { Display as default };
